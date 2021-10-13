897ef2078669df48fcb10bd46ec6502827cabb46
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-13 07:52:52 -0500

    Testing the cli configuration.

--- django_project_config/cli.py
+++ django_project_config/cli.py
@@ -1,31 +1,26 @@
 """Console script for django_project_config."""
 import argparse
 import sys
-from pathlib import Path
-
-from django_project_config.heroku.configuration import run_heroku_config
 
 
 def main():
-    """Console script for django_project_config."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
+    # Create the parser
+    my_parser = argparse.ArgumentParser(description='Configure Django project')
 
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "django_project_config.cli.main")
-    ROOT_FOLDER = Path(__file__).parent.parent.parent
-    print(f'>>> Root: {ROOT_FOLDER}')
-    args = dict()
-    args['verbose'] = True
-    args['base_slug'] = 'home_auto_507_pty'
-    args['environment'] = 'staging'
-    args['target_folder'] = ROOT_FOLDER / 'output'
-    args['create_heroku_app'] = False
-    args['create_postgresql_db'] = True
-    run_heroku_config(**args)
+    # Add the arguments
+    my_parser.add_argument('command', metavar='command', type=str, help='Command')
+    my_parser.add_argument('component', metavar='component', type=str,
+                           help='Compponent to apply the comand to')
+
+    # Execute the parse_args() method
+    args = my_parser.parse_args()
+    print(f'>>> {args.command} {args.component}')
+    name = input('Create an app in heroku [y/n]')
+    print(name)
 
 
 if __name__ == "__main__":
+    """
+    django_project_config config heroku --app heroku-app-name --env staging 
+    """
     sys.exit(main())  # pragma: no cover
---
+++ django_project_config/heroku/django.py
@@ -0,0 +1,6 @@
+def create_super_user():
+    pass
+
+
+if __name__ == '__main__':
+    create_super_user()
