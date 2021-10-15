c62264ccd2b3981916d8b6a40a5ac1046af87d5d
Luis C. Berrocal <luis.berrocal.1942@gmail.com>
2021-10-14 22:13:47 -0500

    Cli capture

--- django_project_config/heroku/utils.py
+++ django_project_config/heroku/utils.py
@@ -1,4 +1,4 @@
-from django_project_config.utils import run_command
+from django_project_config.utils import run_command, Colors
 
 
 def get_variables(app_name, **kwargs):
@@ -19,24 +19,38 @@ def get_variables(app_name, **kwargs):
 def capture_input(*args, **kwargs):
     data = dict()
     data['base_slug'] = {'question': 'Heroku app base name', 'default': 'my-crazy-project'}
-    data['environment'] = {'question': 'Environment', 'default': 'staging'}
-    #data['target_folder'] = ROOT_FOLDER / 'output'
-    #data['create_heroku_app'] = False
-    #data['create_postgresql_db'] = False
-    #data['create_redis'] = False
-    #data['set_aws_variables'] = False
-    #data['set_secrets'] = False
-    #data['create_celery_broker_url'] = False
-    #data['other'] = True
-    #data['bucket_name'] = 'home-automation-staging-bucket'
-    #data['aws_access_file'] = ROOT_FOLDER / 'output/home-automation-staging-user-access.json'
-    #data['run_folder'] = ROOT_FOLDER
+    data['environment'] = {'question': 'Environment', 'default': 'staging', 'choices': ['production', 'staging']}
+    data['target_folder'] = {'question': 'Target folder', 'default': 'output'}
+    data['create_heroku_app'] = {'question': 'Create Heroku app',
+                                 'default': 'Y', 'choices': ['y', 'n']}
+
+    # data['create_heroku_app'] = False
+    # data['create_postgresql_db'] = False
+    # data['create_redis'] = False
+    # data['set_aws_variables'] = False
+    # data['set_secrets'] = False
+    # data['create_celery_broker_url'] = False
+    # data['other'] = True
+    # data['bucket_name'] = 'home-automation-staging-bucket'
+    # data['aws_access_file'] = ROOT_FOLDER / 'output/home-automation-staging-user-access.json'
+    # data['run_folder'] = ROOT_FOLDER
     for k, question in data.items():
-        msg = f"{question['question']} ({question['default']})"
+        if question.get('choices'):
+            display_choices = [x for x in question['choices']]
+            for i, choice in enumerate(display_choices):
+                if choice.upper() == question['default'].upper():
+                    display_choices[i] = f'{Colors.BOLD}{Colors.OKGREEN}{question["default"]}{Colors.ENDC}'
+            choices = f'({", ".join(display_choices)})'
+            msg = f"{question['question']} {choices}: "
+        else:
+            msg = f"{question['question']} ({question['default']}): "
         question['value'] = input(msg)
+        if question['value'] == '':
+            question['value'] = question['default']
+
     print(data)
 
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 #    app_name = 'home-auto-507-pty-staging'
 #    h_vars = get_variables(app_name)
 #    print(h_vars)
